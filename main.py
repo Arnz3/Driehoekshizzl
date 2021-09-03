292994f480dc87a922311c7b0b637b84c972def9
Arnz3 <cuyversarno@gmail.com>
2021-09-03 19:35:20 +0200

    outerpoints collected

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-A = (-6.35,-2.15)
-B = (-2,4)
-C = (3.5,-1)
+import math
 
-D = (2.4,0)
+POINTS = []
+
 
 def isInTriangle(pointA, pointB, pointC, pointD):
     #make vectors
@@ -25,6 +24,57 @@ def isInTriangle(pointA, pointB, pointC, pointD):
 
     else:
         return True
+
+
+def distance(A, B, V): # distance between point and line
+    pass
+
+
+# find outer x coords and y coords
+highestX = (0,0)
+lowestX = (0,0)
+highestY = (0,0)
+lowestY = (0,0)
+
+for point in POINTS:
+    if point[0] > highestX[0]: # highest x coords
+        highestX = point
+    elif point[0] < lowestX[0]: # lowest x coords
+        lowestX = point
+
+    if point[1] > highestY[1]: # highest y coords
+        highestY = point
+    elif point[1] < lowestY[1]: # lowest y coords
+        lowestY = point    
+
+
+# delete points in up and down triangle
+for point in POINTS:
+    if isInTriangle(lowestX, highestY, highestX, point):
+        POINTS.remove(point)
+    
+    if isInTriangle(lowestX, lowestY, highestX, point):
+        POINTS.remove(point)
     
 
-print(isInTriangle(A,B,C,D))
+# sort resting points per kwadrant
+kw1 = []
+kw2 = []
+kw3 = []
+kw4 = []
+
+for point in POINTS:
+    if point[0] < highestY[0] and point[1] > lowestX[1]:
+        kw1.append(point)
+    elif point[0] > highestY[0] and point[1] > highestX[1]:
+        kw2.append(point)
+    elif point[0] > lowestY[0] and point[1] < highestX[1]:
+        kw3.append(point)
+    elif point[0] < lowestY[0] and point[1] < lowestX[1]:
+        kw3.append(point) 
+
+# calculating each kwadrant
+#--> kw1
+for point in kw1:
+    pass
+
