b263c75f6376f0a730cd2f3c65714fee2cd0ba00
Arnz3 <cuyversarno@gmail.com>
2021-09-20 21:52:28 +0200

    added loop still fix needed

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import math
 
-POINTS = []
+POINTS = [(7,2),(4,3),(2,5),(3,7),(12,6),(9,8),(15,5),(12,4),(8,1),(9,5),(7,5),(6,6),(7,7),(8,9),(13,8),(13,2),(10,3),(5,8),(14,7),(14,3)]
+
+
+outerPoints = []
+pointsToDel = []
+Done = False
 
 
 def isInTriangle(pointA, pointB, pointC, pointD):
@@ -30,24 +35,18 @@ def distance(A, B, V): # distance between point and line
     return abs((B[0]-A[0])*(A[1]-V[1])-(A[0]-V[0])*(B[1]-A[1])) / math.sqrt((B[0]-A[0])**2 + (B[1]-A[1])**2)
 
 
-def pointIsClockwise(A, B, V):
-    v1 = (B[0]-A[0], B[1]-A[1])
-    v2 = (B[0]-V[0], B[1]-V[1])
-    cp = v1[0]*v2[1] - v1[1]*v2[0]
-
-    if cp > 0:
-        return False
-    elif cp < 0:
-        return True
-    elif cp == 0:
-        Exception("crossproduct is 0")
+def angleBetweenVectors(A, B, C): #Angle between 2 vectors with A common point
+    v1 = (A[0]-B[0], A[1]-B[1])
+    v2 = (A[0]-C[0], A[1]-C[1])
+    res = ((v1[0]*v2[0])+(v1[1]*v2[1])) / ((math.sqrt(v1[0]**2 + v1[1]**2))*(math.sqrt(v2[0]**2 + v2[1]**2)))
+    return math.acos(res)
 
 
 # find outer x coords and y coords
 highestX = (0,0)
-lowestX = (0,0)
+lowestX = (99999999999999999,99999999999999999)
 highestY = (0,0)
-lowestY = (0,0)
+lowestY = (99999999999999999,99999999999999999)
 
 for point in POINTS:
     if point[0] > highestX[0]: # highest x coords
@@ -58,64 +57,69 @@ for point in POINTS:
     if point[1] > highestY[1]: # highest y coords
         highestY = point
     elif point[1] < lowestY[1]: # lowest y coords
-        lowestY = point    
+        lowestY = point
+
+outerPoints.append(lowestX)    
+outerPoints.append(highestY)    
+outerPoints.append(highestX)    
+outerPoints.append(lowestY)
+
+POINTS.remove(lowestY)
+POINTS.remove(lowestX)
+POINTS.remove(highestY)
+POINTS.remove(highestX)
 
 
 # delete points in up and down triangle
 for point in POINTS:
     if isInTriangle(lowestX, highestY, highestX, point):
-        POINTS.remove(point)
+        pointsToDel.append(point)
     
-    if isInTriangle(lowestX, lowestY, highestX, point):
-        POINTS.remove(point)
+    elif isInTriangle(lowestX, lowestY, highestX, point):
+        pointsToDel.append(point)
     
+for item in pointsToDel:
+    POINTS.remove(item)
+pointsToDel.clear()
 
-# sort resting points per kwadrant
-'''
-kw1 = []
-kw2 = []
-kw3 = []
-kw4 = []
+#create relative middle point
+relMiddle = (round((highestX[0] + lowestX[0])/2) , round((highestY[1] + lowestY[1])/2))
 
-for point in POINTS:
-    if point[0] < highestY[0] and point[1] > lowestX[1]:
-        kw1.append(point)
-    elif point[0] > highestY[0] and point[1] > highestX[1]:
-        kw2.append(point)
-    elif point[0] > lowestY[0] and point[1] < highestX[1]:
-        kw3.append(point)
-    elif point[0] < lowestY[0] and point[1] < lowestX[1]:
-        kw3.append(point) 
-'''
 
-# calculating each kwadrant
-#--> kw1
-def calcKwadrant(A , B , clockwise):
+startpoint = outerPoints[0]
+
+while not Done:
+    smollestAngle = 6.57
     workingPoints = []
-
+    for point in outerPoints:
+        angle = angleBetweenVectors(relMiddle, startpoint, point)
+        if angle > 0 and angle < smollestAngle and point != startpoint:
+            nextPoint = point
+        
     for point in POINTS:
-        if point == A or point == B:
-            pass
-        elif pointIsClockwise(A, B, point) == clockwise:
+        angle = angleBetweenVectors(relMiddle, startpoint, point)
+        if angle > 0 and angle < smollestAngle:
             workingPoints.append(point)
 
-    longestDist = 0
+    largestDist = 0
     for point in workingPoints:
-        if distance(A, B, point) > longestDist:
-            longestDist = distance(A, B, point)
-            fardestPoint = point
+        if distance(startpoint, nextPoint, point) > largestDist:
+            farestPoint = point
 
-    workingPoints.remove(fardestPoint)
+    workingPoints.remove(farestPoint)
+    outerPoints.append(farestPoint)
+    POINTS.remove(farestPoint)
 
     for point in workingPoints:
-        if isInTriangle(A, B, fardestPoint, point):
+        if isInTriangle(startpoint, farestPoint, nextPoint, point):
             POINTS.remove(point)
-            workingPoints.remove(point)
-    
-    if not workingPoints:
-        return
-    else:
-        pass # TODO figure out wtf i have to do
-    
 
+    if not POINTS:
+        Done = True
+
+    startpoint = nextPoint
+
+
+
+print (outerPoints)
 
